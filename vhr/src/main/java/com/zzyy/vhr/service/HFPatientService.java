3153ebccb9dcd06adef49aa450155581923550bd
lenovo717 <534145298@qq.com>
2020-12-17 10:05:29 +0800

    1.0.0

--- vhr/src/main/java/com/zzyy/vhr/service/HFPatientService.java
+++ vhr/src/main/java/com/zzyy/vhr/service/HFPatientService.java
@@ -1,24 +1,31 @@
 package com.zzyy.vhr.service;
 
-import com.zzyy.vhr.mapper2.HFPatientMapper;
+
+import com.zzyy.vhr.mapper.zzyy_hf_patientMapper;
 import com.zzyy.vhr.model.RespPageBean;
-import com.zzyy.vhr.model2.HFPatient;
+import com.zzyy.vhr.model2.zzyy_hf_patient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class HFPatientService {
 
     @Autowired
-    HFPatientMapper patientMapper;
+    zzyy_hf_patientMapper patientMapper;
 
     public RespPageBean getPatientByPage(Integer page, Integer size, String keyword) {
         if(page !=null && size !=null){
             page = (page-1) *size;
         }
-        List<HFPatient> data = patientMapper.getPatientByPage(page,size,keyword);
+        Map<String, Object> qryData = new HashMap<String, Object>();
+        qryData.put("pageIndex",page);
+        qryData.put("size",size);
+        qryData.put("keyword",keyword);
+        List<zzyy_hf_patient> data = patientMapper.getPatientByPage(qryData);
         //Long total = employeeMapper.getTotal(keyword);
         RespPageBean bean = new RespPageBean();
         bean.setData(data);
@@ -27,8 +34,21 @@ public class HFPatientService {
     }
 
 
-    public HFPatient getPatientDetail(String patient_id)
+    public zzyy_hf_patient getPatientDetail(String patient_id)
     {
         return patientMapper.getPatientDetail(patient_id);
     }
+
+    public int addHFPatient(zzyy_hf_patient patient) {
+        patient.setSex(1);
+
+        return patientMapper.insert(patient);
+    }
+
+    public zzyy_hf_patient getPatientBySocialNo(String social_no)
+    {
+        return patientMapper.getPatientBySocialNo(social_no);
+    }
+
+
 }
