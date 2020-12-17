dc272eada55715f7f82cfc2ae392089247f96406
lenovo717 <534145298@qq.com>
2020-12-17 09:20:09 +0800

    1.0.0

---
+++ vhr/src/main/java/com/zzyy/vhr/model2/zzyy_hf_patient.java
@@ -0,0 +1,145 @@
+package com.zzyy.vhr.model2;
+
+public class zzyy_hf_patient {
+    private Integer id;
+
+    private String fileId;
+
+    private String name;
+
+    private Integer sex;
+
+    private String hicNo;
+
+    public String getHicNo() {
+        return hicNo;
+    }
+
+    public void setHicNo(String hicNo) {
+        this.hicNo = hicNo;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getVocation() {
+        return vocation;
+    }
+
+    public void setVocation(String vocation) {
+        this.vocation = vocation;
+    }
+
+    private String socialNo;
+
+    private String birthday;
+
+    private String vocation;
+
+    private String education;
+
+    private String phone;
+
+    private String address;
+
+    private String linkMan;
+
+    private String linkPhone;
+
+
+
+    private Integer result;
+
+    public Integer getResult() {
+        return result;
+    }
+
+    public void setResult(Integer result) {
+        this.result = result;
+    }
+
+    public String getFile_id() {
+        return fileId;
+    }
+
+    public void setFile_id(String fileId) {
+        this.fileId = fileId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public String getSocialNo() {
+        return socialNo;
+    }
+
+    public void setSocialNo(String socialNo) {
+        this.socialNo = socialNo;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLinkMan() {
+        return linkMan;
+    }
+
+    public void setLinkMan(String linkMan) {
+        this.linkMan = linkMan;
+    }
+
+    public String getLinkPhone() {
+        return linkPhone;
+    }
+
+    public void setLinkPhone(String linkPhone) {
+        this.linkPhone = linkPhone;
+    }
+}
---
+++ vhr/src/main/java/com/zzyy/vhr/model2/zzyy_hf_patient_file_new.java
@@ -0,0 +1,73 @@
+package com.zzyy.vhr.model2;
+
+public class zzyy_hf_patient_file_new {
+    private Integer id;
+
+    private String fileId;
+
+    private String patientId;
+
+    private String className;
+
+    private String item;
+
+    private String value;
+
+    private String remark;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(String fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
