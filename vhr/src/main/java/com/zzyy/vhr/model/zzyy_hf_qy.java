2456d0eebe6d29f8f0714b2206b4e8af44ce9b99
lenovo717 <534145298@qq.com>
2020-12-17 09:24:26 +0800

    1.0.0

---
+++ vhr/src/main/java/com/zzyy/vhr/model/zzyy_hf_qy.java
@@ -0,0 +1,53 @@
+package com.zzyy.vhr.model;
+
+public class zzyy_hf_qy {
+    private Integer id;
+
+    private String colName;
+
+    private String colClass;
+
+    private String colType;
+
+    private String colVal;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getColName() {
+        return colName;
+    }
+
+    public void setColName(String colName) {
+        this.colName = colName;
+    }
+
+    public String getColClass() {
+        return colClass;
+    }
+
+    public void setColClass(String colClass) {
+        this.colClass = colClass;
+    }
+
+    public String getColType() {
+        return colType;
+    }
+
+    public void setColType(String colType) {
+        this.colType = colType;
+    }
+
+    public String getColVal() {
+        return colVal;
+    }
+
+    public void setColVal(String colVal) {
+        this.colVal = colVal;
+    }
+}
